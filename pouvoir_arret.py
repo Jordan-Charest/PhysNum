b5125931815a6dbe6cfcaa2035a40e44846e5e8f
Jordan-Charest <jordan.charest.2@ulaval.ca>
2022-01-27 13:54:49 -0500

    test pouvoir arret

---
+++ pouvoir_arret.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Variables du problème
+
+re = 1      # rayon classique de l'électron
+ne = 1      # densité électronique du matériau
+Z = 1       # charge de la particule
+I = 1       # énergie moyenne d'excitation du matériau
+beta = 1    # facteur de Lorentz
+gamma = 1   # facteur de Lorentz
+delta = 1   # terme considérant les effets de la polarisation
+???
+L1 = 1      # coefficient de correction de Barkas
+L2 = 1      # coefficient de correction de Bloch
