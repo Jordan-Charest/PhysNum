0f1029938d76e4fa379387a386f8316308e35492
Jordan-Charest <jordan.charest.2@ulaval.ca>
2022-01-28 13:39:39 -0500

    28/01 13:39

--- constantes.py
+++ constantes.py
@@ -2,13 +2,12 @@
 # BUT: définir les constantes utilisées dans les autres scripts
 ###
 
-c = 3 * 10**8      # vitesse de la lumière [m/s]
-re = 1      # rayon classique de l'électron [m]
-me = 1      # masse au repos de l'électron [MeV]
-mp = 1      # masse au repos du proton [MeV]
+c = 3 * 10**8                   # vitesse de la lumière [m/s]
+re = 2.8179 * 10**(-15)         # rayon classique de l'électron [m]
+me = 0.511      # masse au repos de l'électron [MeV]
+mp = 938.27231      # masse au repos du proton [MeV]
 Z = 1       # charge de la particule
 I = 1       # énergie moyenne d'excitation du matériau
-beta = 1    # facteur de Lorentz
 delta = 1   # terme considérant les effets de la polarisation
 L1 = 1      # coefficient de correction de Barkas
 L2 = 1      # coefficient de correction de Bloch
