3eac6463cfc5932d97737d140a107a11d61dfa25
Jordan-Charest <jordan.charest.2@ulaval.ca>
2022-01-29 13:16:29 -0500

    Update constantes.py
    constantes update

--- constantes.py
+++ constantes.py
@@ -2,10 +2,10 @@
 # BUT: définir les constantes utilisées dans les autres scripts
 ###
 
-c = 3 * 10**8                   # vitesse de la lumière [m/s]
-re = 2.8179 * 10**(-15)         # rayon classique de l'électron [m]
-me = 0.511      # masse au repos de l'électron [MeV]
-mp = 938.27231      # masse au repos du proton [MeV]
+c = 2.99792458 * 10**8                   # vitesse de la lumière [m/s]
+re = 2.8179409238 * 10**(-15)         # rayon classique de l'électron [m]
+me = 0.510990615      # masse au repos de l'électron [MeV]
+mp = 938.2723128      # masse au repos du proton [MeV]
 Z = 1       # charge de la particule
 I = 1       # énergie moyenne d'excitation du matériau
 delta = 1   # terme considérant les effets de la polarisation
