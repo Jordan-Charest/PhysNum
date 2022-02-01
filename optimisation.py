64919421250fd7adb0790fa1a666c51e92ad9e6f
Benjamin Claveau <benjamin.claveau.1@ulaval.ca>
2022-02-01 15:36:23 -0400

    Q9

--- optimisation.py
+++ optimisation.py
@@ -5,4 +5,6 @@ import matplotlib.pyplot as plt
 sample = moyal(loc=150, scale=4).rvs(size=10000)
 
 plt.hist(sample)
+plt.xlabel('Énergie cinétique initiale [MeV]')
+plt.ylabel('Nombre de protons')
 plt.show()
