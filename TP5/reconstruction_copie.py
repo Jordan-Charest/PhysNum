898a20d24d8e81662b9e2580bd24ab197acd8d46
becla18 <benjamin.claveau.1@ulaval.ca>
2022-04-03 16:39:49 -0300

    Q2 code fonctionnel

--- TP5/reconstruction_copie.py
+++ TP5/reconstruction_copie.py
@@ -12,7 +12,7 @@ import time
 # local files
 import geometry as geo
 import util as util
-import CTfilter as CTfilter
+#import CTfilter as CTfilter
 
 ## créer l'ensemble de données d'entrée à partir des fichiers
 def readInput():
@@ -52,65 +52,21 @@ def laminogram():
         print("working on image column: "+str(j+1)+"/"+str(geo.nbvox))
         for i in range(geo.nbvox): # lignes de l'image
             for a in range(len(angles)):
-                r = np.sqrt(i**2 + j**2)
-                theta_vox = np.arctan(j / i)
+                x = (j - geo.nbvox / 2) * geo.voxsize
+                y = (i - geo.nbvox / 2) * geo.voxsize
+                r = np.sqrt(x**2 + y**2)
+                if y < 0:
+                    theta_vox = np.arctan(x / y)
+                elif y == 0 and x <= 0:
+                    theta_vox = np.pi / 2
+                elif y == 0 and x > 0:
+                    theta_vox = 3 * np.pi / 2
+                else:
+                    theta_vox = np.arctan(x / y) + np.pi
                 theta = theta_vox - angles[a]
-                distance_rayon = r * np.sin(theta) / 0.165
+                distance_rayon = r * np.sin(theta) / geo.pixsize
                 indice_rayon = int(geo.nbpix / 2 + np.round(distance_rayon))
+
                 image[i, j] += sinogram[a][indice_rayon]
                 
     util.saveImage(image, "lam")
-
-
-## reconstruire une image TDM en mode retroprojection filtrée
-def backproject():
-    
-    [nbprj, angles, sinogram] = readInput()
-    
-    # initialiser une image reconstruite
-    image = np.zeros((geo.nbvox, geo.nbvox))
-    
-    ### option filtrer ###
-    CTfilter.filterSinogram(sinogram)
-    ######
-    
-    # "etaler" les projections sur l'image
-    # ceci sera fait de façon "voxel-driven"
-    # pour chaque voxel, trouver la contribution du signal reçu
-    for j in range(geo.nbvox): # colonnes de l'image
-        print("working on image column: "+str(j+1)+"/"+str(geo.nbvox))
-        for i in range(geo.nbvox): # lignes de l'image
-            for a in range(len(angles)):
-                #votre code ici
-               
-    
-    util.saveImage(image, "fbp")
-
-
-## reconstruire une image TDM en mode retroprojection
-def reconFourierSlice():
-    
-    [nbprj, angles, sinogram] = readInput()
-
-    # initialiser une image reconstruite, complexe
-    # pour qu'elle puisse contenir sa version FFT d'abord
-    IMAGE = np.zeros((geo.nbvox, geo.nbvox), 'complex')
-    
-    # conteneur pour la FFT du sinogramme
-    SINOGRAM = np.zeros(sinogram.shape, 'complex')
-
-    #image reconstruite
-    image = np.zeros((geo.nbvox, geo.nbvox))
-    #votre code ici
-   
-    
-    util.saveImage(image, "fft")
-
-
-## main ##
-start_time = time.time()
-laminogram()
-#backproject()
-#reconFourierSlice()
-print("--- %s seconds ---" % (time.time() - start_time))
-
