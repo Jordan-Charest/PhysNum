6e2f95b78ea9e4970847f9ef7f3fa3be5d01986a
Jordan-Charest <73846414+Jordan-Charest@users.noreply.github.com>
2022-04-23 17:24:23 -0400

    TP5 fini

--- TP5/reconstruction.py
+++ TP5/reconstruction.py
@@ -8,11 +8,12 @@
 # libs
 import numpy as np
 import time
+import math
 
 # local files
 import geometry as geo
 import util as util
-import CTfiltre as CTfilter
+import CTfiltre as CTfiltre
 
 ## créer l'ensemble de données d'entrée à partir des fichiers
 def readInput():
@@ -52,11 +53,21 @@ def laminogram():
         print("working on image column: "+str(j+1)+"/"+str(geo.nbvox))
         for i in range(geo.nbvox): # lignes de l'image
             for a in range(len(angles)):
-                r = np.sqrt(i**2 + j**2)
-                theta_vox = np.arctan(j / i)
+                x = (j - geo.nbvox / 2) * geo.voxsize
+                y = (i - geo.nbvox / 2) * geo.voxsize
+                r = np.sqrt(x**2 + y**2)
+                if y < 0:
+                    theta_vox = np.arctan(x / y)
+                elif y == 0 and x <= 0:
+                    theta_vox = np.pi / 2
+                elif y == 0 and x > 0:
+                    theta_vox = 3 * np.pi / 2
+                else:
+                    theta_vox = np.arctan(x / y) + np.pi
                 theta = theta_vox - angles[a]
-                distance_rayon = r * np.sin(theta) / 0.165
+                distance_rayon = r * np.sin(theta) / geo.pixsize
                 indice_rayon = int(geo.nbpix / 2 + np.round(distance_rayon))
+
                 image[i, j] += sinogram[a][indice_rayon]
                 
     util.saveImage(image, "lam")
@@ -71,7 +82,7 @@ def backproject():
     image = np.zeros((geo.nbvox, geo.nbvox))
     
     ### option filtrer ###
-    CTfilter.filterSinogram(sinogram)
+    CTfiltre.filterSinogram(sinogram)
     ######
     
     # "etaler" les projections sur l'image
@@ -81,36 +92,70 @@ def backproject():
         print("working on image column: "+str(j+1)+"/"+str(geo.nbvox))
         for i in range(geo.nbvox): # lignes de l'image
             for a in range(len(angles)):
-                #votre code ici
-               
+                x = (j - geo.nbvox / 2) * geo.voxsize
+                y = (i - geo.nbvox / 2) * geo.voxsize
+                r = np.sqrt(x**2 + y**2)
+                if y < 0:
+                    theta_vox = np.arctan(x / y)
+                elif y == 0 and x <= 0:
+                    theta_vox = np.pi / 2
+                elif y == 0 and x > 0:
+                    theta_vox = 3 * np.pi / 2
+                else:
+                    theta_vox = np.arctan(x / y) + np.pi
+                theta = theta_vox - angles[a]
+                distance_rayon = r * np.sin(theta) / geo.pixsize
+                indice_rayon = int(geo.nbpix / 2 + np.round(distance_rayon))
+
+                image[i, j] += sinogram[a][indice_rayon]
     
     util.saveImage(image, "fbp")
 
 
-## reconstruire une image TDM en mode retroprojection
-def reconFourierSlice():
+## reconstruire une image TDM en mode retroprojection filtrée en déterminant 
+## les valeurs d'atténation par interpolation linéaire
+def backproject_interpol():
     
     [nbprj, angles, sinogram] = readInput()
-
-    # initialiser une image reconstruite, complexe
-    # pour qu'elle puisse contenir sa version FFT d'abord
-    IMAGE = np.zeros((geo.nbvox, geo.nbvox), 'complex')
     
-    # conteneur pour la FFT du sinogramme
-    SINOGRAM = np.zeros(sinogram.shape, 'complex')
-
-    #image reconstruite
+    # initialiser une image reconstruite
     image = np.zeros((geo.nbvox, geo.nbvox))
-    #votre code ici
-
     
-    util.saveImage(image, "fft")
+    ### option filtrer ###
+    CTfiltre.filterSinogram(sinogram)
+    ######
+    
+    # "etaler" les projections sur l'image
+    # ceci sera fait de façon "voxel-driven"
+    # pour chaque voxel, trouver la contribution du signal reçu
+    for j in range(geo.nbvox): # colonnes de l'image
+        print("working on image column: "+str(j+1)+"/"+str(geo.nbvox))
+        for i in range(geo.nbvox): # lignes de l'image
+            for a in range(len(angles)):
+                x = (j - geo.nbvox / 2) * geo.voxsize
+                y = (i - geo.nbvox / 2) * geo.voxsize
+                r = np.sqrt(x**2 + y**2)
+                if y < 0:
+                    theta_vox = np.arctan(x / y)
+                elif y == 0 and x <= 0:
+                    theta_vox = np.pi / 2
+                elif y == 0 and x > 0:
+                    theta_vox = 3 * np.pi / 2
+                else:
+                    theta_vox = np.arctan(x / y) + np.pi
+                theta = theta_vox - angles[a]
+                distance_rayon = r * np.sin(theta) / geo.pixsize
+                if distance_rayon > 0:
+                    indice_rayon1 = int(geo.nbpix / 2 + math.floor(distance_rayon))
+                    indice_rayon2 = int(geo.nbpix / 2 + math.ceil(distance_rayon))
+                else:
+                    indice_rayon1 = int(geo.nbpix / 2 + math.ceil(distance_rayon))
+                    indice_rayon2 = int(geo.nbpix / 2 + math.floor(distance_rayon))
+                r1 = sinogram[a][indice_rayon1]
+                r2 = sinogram[a][indice_rayon2]
+                d = abs(distance_rayon) % 1 * geo.pixsize
+                attenuation = geo.interpol_lin(r1, r2, d)
 
-
-## main ##
-start_time = time.time()
-laminogram()
-#backproject()
-#reconFourierSlice()
-print("--- %s seconds ---" % (time.time() - start_time))
-
+                image[i, j] += attenuation
+    
+    util.saveImage(image, "fbp")
